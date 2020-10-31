46a0ffd79b75658452bf7d3c073294aa48e68a4c
Brad <bcoffield@gmail.com>
2020-10-31 11:55:45 -0600

    init from cheatsheets-quasar

---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/Main.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/login', component: () => import('pages/Login.vue') },
+      { path: '/list-cheatsheets', component: () => import('pages/list-cheatsheets.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
