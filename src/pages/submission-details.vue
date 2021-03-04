2aec1ee662e87177e688d7ec1b2ee14c18e9c473
Brad <bcoffield@gmail.com>
2021-03-04 11:33:09 -0700

    yep

--- src/pages/submission-details.vue
+++ src/pages/submission-details.vue
@@ -1,31 +1,69 @@
 <template>
   <q-page>
-     <q-card  class="q-pa-md bg-dark q-mb-xl q-mt-xl text-primary header-card"> <h2 style="">
-      Submission Details
-    </h2></q-card>
-
- <q-card
-      class="my-card text-white bg-dark q-pa-xl"
+    <q-card class="q-pa-md bg-dark q-mb-xl q-mt-xl text-primary header-card">
+      <h2 style="">
+        Submission Details
+      </h2></q-card
     >
-      <q-card-section>
-        <div class="text-h4">{{submission.title}}</div>
-        <div class="text-subtitle1">by {{submission.author}} : <a :href="`mailto:${submission.email}`">{{submission.email}}</a></div>
-      </q-card-section>
-<hr>
-      <q-card-section class="q-pt-none text-body1">
-          <ul>
-              <li><span class="text-weight-bold" >Genre:</span> {{submission.genre}} </li>
-              <li><span class="text-weight-bold">Author's Primary Genre:</span> {{submission.primary_genre}}</li>
-              <li><span class="text-weight-bold">Author's Letter/Bio:</span> {{submission.author_letter}}</li>
-              <li><span class="text-weight-bold">Link to the piece: </span> <a :href="submission.file" target="_blank">{{submission.file}}</a></li>
-       
-          </ul>
-       i think still in this card a v-for series of sub-cards of Staff Ratings
-              </q-card-section>
- 
-    </q-card>
 
- 
+    <q-card class="my-card text-white bg-dark q-pa-xl">
+      <q-card-section>
+        <div class="text-h4">{{ submission.title }}</div>
+        <div class="text-subtitle1">
+          by {{ submission.author }} :
+          <a :href="`mailto:${submission.email}`">{{ submission.email }}</a>
+        </div>
+      </q-card-section>
+      <hr />
+      <q-card-section class="q-pt-none text-body1">
+        <ul>
+          <li>
+            <span class="text-weight-bold">Genre:</span> {{ submission.genre }}
+          </li>
+          <li>
+            <span class="text-weight-bold">Author's Primary Genre:</span>
+            {{ submission.primary_genre }}
+          </li>
+          <li>
+            <span class="text-weight-bold">Author's Letter/Bio:</span>
+            {{ submission.author_letter }}
+          </li>
+          <li>
+            <span class="text-weight-bold">Notes:</span>
+            {{ submission.notes }}
+          </li>
+
+          <li>
+            <span class="text-weight-bold">Link to the piece: </span>
+            <a :href="submission.file" target="_blank">{{ submission.file }}</a>
+          </li>
+        </ul>
+
+        <q-btn
+          color="accent"
+          text-color="white"
+          label="RATE THIS SUBMISSION"
+          class="full-width q-mt-xl"
+          @click="reviewItem"
+        />
+      </q-card-section>
+    </q-card>
+    <q-card class="my-card text-white bg-dark q-pa-xl">
+      <h5 class="q-mb-xs">Existing Ratings</h5>
+      <hr class="q-mb-lg" />
+      <template v-if="emptyRatings == false"
+        >No one has rated this submission yet.</template
+      >
+      <template v-if="emptyRatings == true">
+        <ul>
+          <li v-for="item in submission.ratings" v-bind:key="item.name" style="display:inline">
+            Name: {{item.raterName}} <br>
+            Rating: {{item.ratingNumber}} <br>
+            Rating Notes: {{item.ratingNotes}}
+          </li>
+        </ul>
+      </template>
+    </q-card>
   </q-page>
 </template>
 
@@ -35,6 +73,7 @@ export default {
   data() {
     return {
       key: this.$route.params.id,
+      emptyRatings: true,
       submission: {
         author: "",
         email: "",
@@ -57,7 +96,10 @@ export default {
       .then(doc => {
         if (doc.exists) {
           this.submission = doc.data();
-          console.log(this.submission);
+          if (doc.data().ratings.length > 0) {
+            this.emptyRatings = false;
+          }
+          console.log("submission data:", this.submission);
           console.log(this.$route.params.id);
         } else {
           alert("No such document!");
@@ -66,33 +108,11 @@ export default {
   },
 
   methods: {
-    getDate() {
-      let date = new Date();
-      return date.toISOString();
-    },
-    sendSub(evt) {
-      evt.preventDefault();
-      this.submission.updated = this.getDate();
-      this.ref
-        .doc(this.$route.params.id)
-        .set(this.submission, { merge: true })
-        .then(function() {
-          console.log("Document successfully written!");
-        })
-        .catch(function(error) {
-          console.error("Error writing document: ", error);
-        });
-    },
-    onReset() {
-      this.submission.author = null;
-      this.submission.email = null;
-      this.submission.file = null;
-      this.submission.updated = null;
-      this.submission.notes = null;
-      this.submission.title = null;
-      this.submission.author_letter = null;
-      this.submission.genre = null;
-      this.submission.primary_genre = null;
+    reviewItem() {
+      this.$router.push({
+        name: "rate-submission",
+        params: { id: this.key }
+      });
     }
   }
 };
