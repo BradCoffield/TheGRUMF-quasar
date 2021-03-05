fba8168a52ca3b6f2e440e986ac720d9f2f2b8ac
Brad <bcoffield@gmail.com>
2021-03-05 08:51:30 -0700

    small text edit to delete dialog

--- src/components/DeleteDialog.vue
+++ src/components/DeleteDialog.vue
@@ -33,7 +33,7 @@
         </q-card-section>
 
         <q-card-actions align="right">
-          <q-btn flat label="Cancel" color="secondary" v-close-popup />
+          <q-btn flat label="Close" color="secondary" v-close-popup />
         </q-card-actions>
       </q-card>
     </q-dialog>
